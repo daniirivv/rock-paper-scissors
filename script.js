4f83679146c8351b2a7281d5d58d8439c40a3c5f
Daniel Rivera <danielriveranunezdearenas@outlook.com>
2025-10-25 19:41:18 +0200

    Implement playRound function

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const rock = "ROCK";
 const paper = "PAPER";
 const scissors = "SCISSORS"
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const intToChoiceMap = new Map([
         [0, rock],
@@ -21,7 +24,7 @@ function getHumanChoice() {
     let correctInput;
     let input;
     do {
-        input = prompt("Make a choice: ", "ROCK || PAPER || SCISSORS");
+        input = String.prototype.toUpperCase(prompt("Make a choice: ", "ROCK || PAPER || SCISSORS"));
         correctInput = validInput(input);
         if(correctInput === false){
             console.log("Only ROCK, PAPER or SCISSORS are valid inputs");
@@ -29,4 +32,40 @@ function getHumanChoice() {
     } while (!correctInput);
 
     return input;
+}
+
+function isWinner(humanChoice, computerChoice){
+    const beatingRules = new Map([
+        [rock, scissors],
+        [paper, rock],
+        [scissors, paper]
+    ]);
+
+    switch (computerChoice) {
+        case beatingRules.get(humanChoice):
+            return 1;
+        case humanChoice:
+            return 0;
+        default:
+            return -1;
+    }
+}
+
+function playRound(humanChoice, computerChoice){
+    switch (isWinner(humanChoice, computerChoice)) {
+        case 1:
+            console.log("You win! " + humanChoice + " beats " + computerChoice);
+            humanScore++;
+            break;
+        case 0:
+            console.log("It's a tie!");
+            break;
+        case -1:
+            console.log("You lose! " + computerChoice + " beats " + humanChoice);
+            computerScore++;
+            break;
+        default:
+            console.log("idk what happened :)");
+            break;
+    }
 }
